c825912eb192fd7903157005422f254829516708
Anoop Moothedath <anoop98moothedath@gmail.com>
2020-02-02 13:06:59 +0530

    Added the files

---
+++ app/src/main/java/com/example/yourtunes_musicplayer/MusicController.java
@@ -0,0 +1,12 @@
+package com.example.yourtunes_musicplayer;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.MediaController;
+
+public class MusicController extends MediaController {
+    public MusicController(Context context) {
+        super(context);
+    }
+    public void hide(){}
+}
